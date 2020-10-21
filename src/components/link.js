95c0461d97c6c3a74097c69ac4973f1d5c48c8b2
wuben <wuben@elensdata.com>
2020-10-22 00:01:11 +0800

    router-link

--- src/components/link.js
+++ src/components/link.js
@@ -1,3 +1,31 @@
 export default {
-    
+    functional: true,
+    name: 'router-link',
+    props: {
+        to: {
+            type: [String, Object],
+            required: true,
+        },
+        tag: {
+            type: String,
+            default: 'a'
+        }
+    },
+    render(h, { props, parent, children }) {
+        return h(
+            props.tag,
+            {
+                attrs: {
+                    href: props.to
+                },
+                on: {
+                    click(e) {
+                        e.preventDefault()
+                        parent.$router.go(props.to)
+                    }
+                }
+            },
+            children
+        )
+    }
 }
