9cac054d49a879f51fec8e350e3347535cae2b17
wuben <wuben@elensdata.com>
2020-10-22 00:05:56 +0800

    progress: 优化depth算法

--- src/components/view.js
+++ src/components/view.js
@@ -10,20 +10,22 @@ export default {
     render(h, { props, children, parent, data }) {
         const route = parent.$route
          // 标记 routerVie 类型
-        let _parent = parent
         let depth = 0
 
-        while(_parent) {
-            if (_parent.$vnode && _parent.$vnode.data._routerView) {
-                depth ++
+        while(parent) {
+            if (parent.$vnode && parent.$vnode.data.routerView) {
+                depth = parent.$vnode.data.routerViewDepth + 1
+                break
             }
-            _parent = _parent.$parent
+            parent = parent.$parent
         }
-        data._routerView = true
+        data.routerView = true
+        data.routerViewDepth = depth
 
-        const matched = route.matched[depth] && route.matched[depth].components[props.name] || null
+        const matched = route.matched[depth]
+        const component =  matched && matched.components[props.name]
         return h(
-            matched,
+            component,
             data,
             children
         )
